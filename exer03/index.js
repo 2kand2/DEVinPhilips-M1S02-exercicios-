96544a949180c3dc43f39e777867310707bcdf81
Henrique <octaciano11@gmail.com>
2022-10-12 15:25:33 -0300

    Include Exe03 Tabuada

---
+++ exer03/index.js
@@ -0,0 +1,6 @@
+let number = parseFloat(prompt("Digite o número da tabuada que deseja!"));
+
+for (let i = 0; i <= 10; i++) {
+  let result = number * i;
+  alert(number + " x " + i + " = " + result);
+}
