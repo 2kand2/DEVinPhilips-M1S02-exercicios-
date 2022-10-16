08e4e85e50b6b1fa5977fdd64e5be7418f536709
Henrique <octaciano11@gmail.com>
2022-10-16 20:51:10 -0300

    Include exercise of arithmetic sequence calculator

---
+++ index.js
@@ -0,0 +1,11 @@
+let initialValue = parseFloat(prompt("Informe o valor inicial: "));
+let commumDiference = parseFloat(prompt("Informe o valor da razão: "));
+
+let pA = initialValue;
+let text = initialValue.toString();
+for (let i = 0; i < 9; i++) {
+  pA += commumDiference;
+
+  text += "," + pA;
+}
+alert("A p.a é " + text);
