ea71779a1aa452bed42990ed4c0e1cb391d18a50
Octaciano Henrique Peralte Kuhn <99394301+2kand2@users.noreply.github.com>
2022-10-16 23:14:11 -0300

    Atualização do exercicio

--- exer09/index.js
+++ exer09/index.js
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-let qntNumeroPrimo = 0;
-for (let i = 0; i <= 1000; i++) {
-  let divisoes = 0;
-
-  for (let j = 0; j <= i; j++) {
-    if (i % j == 0) {
-      divisoes++;
-    }
-  }
-  if (divisoes == 2) {
-    console.log(i);
-    qntNumeroPrimo++;
-  }
-  console.log("Números de divisões " + divisoes);
-}
-console.log("Existem " + qntNumeroPrimo + " números primos entre 0 e 1000");
-=======
 let initialValue = parseFloat(prompt("Informe o valor inicial: "));
 let commumDiference = parseFloat(prompt("Informe o valor da razão: "));
 
@@ -27,4 +9,4 @@ for (let i = 0; i < 9; i++) {
   text += "," + pA;
 }
 alert("A p.a é " + text);
->>>>>>> exe09calculadoraDePA
+
